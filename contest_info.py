5390068dd2b124c472289f44a2c0b7d49ad75f87
sparshmittal <sparshmittal2311@gmail.com>
2022-12-09 18:30:59 +0530

    rating changes

--- contest_info.py
+++ contest_info.py
@@ -1,15 +1,31 @@
 import db
 import requests
+import json 
 
 URL_BASE = 'https://clist.by/api/v2/'
 clist_token="username=Sparsh&api_key=c5b41252e84b288521c92f78cc70af99464345f8"
 
-async def codeforces_rating_changes():                     # function to get the rating changes of all users in codeforces
+async def contest_id_finder(event_name):                    #function to convert cf contest name to contest id
+    url=URL_BASE+'contest/?'+clist_token+'&resource_id=1'+'&order_by=-start'+'&limit=100'  #url of the to be fetched
+    if(event_name==None):                                                    #if handle is none, return none
+        return None 
+    try:            
+        resp= requests.get(url)                                              #fetching response
+        details=json.dumps(resp.json(),ensure_ascii=False)               #returning id
+        data=json.loads(details)['objects']
+        for contest in data:
+            if contest['href']=='https://codeforces.com/contests/'+event_name:
+                return contest['id']
+    except Exception as e:                                                  #tackiling errors    
+        print(e)
+
+async def codeforces_rating_changes(event_name):                     # function to get the rating changes of all users in codeforces
     codeforces_handle = await db.get_all_codeforces_handles()       # get all the codeforces handles from the database
+    contest_id=await contest_id_finder(event_name)
     try: 
         returnlist=[]
         for handle in codeforces_handle:                                # iterate over all the handles
-            url = URL_BASE+'statistics/?'+clist_token+'&contest_id=38185697'+'&account_id='+str(handle[1])+'&with_problems=True&with_more_fields=True'           # get the rating changes of the user
+            url = URL_BASE+'statistics/?'+clist_token+'&contest_id='+str(contest_id)+'&account_id='+str(handle[1])+'&with_problems=True&with_more_fields=True'           # get the rating changes of the user
             response = requests.get(url).json()
             if response['objects']:
                 if 'CONTESTANT' in response['objects'][0]['more_fields']['participant_type']:
