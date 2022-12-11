92f84a79748cf2421c7828583936e08dfc8a5c6f
Rohan Mitra <rohanmitra2420@gmail.com>
2022-12-11 20:55:55 +0530

    Prototype

--- stalk.py
+++ stalk.py
@@ -1,7 +1,7 @@
 import requests
 from operator import itemgetter
 
-async def stalk_user(ctx,codeforces_handle,hardest):
+async def stalk_user(ctx,codeforces_handle,hardest=False,R=None):
     url = "https://codeforces.com/api/user.status?handle="+str(codeforces_handle)+"&from="+str(1)
     response = requests.get(url,timeout=5).json()
     total=len(response['result'])
@@ -13,6 +13,12 @@ async def stalk_user(ctx,codeforces_handle,hardest):
         if obj['verdict']=='OK':
             n_dict.append({'Prob':str(obj['problem']['name']),'Rating':int(obj['problem']['rating'])})
     if hardest==True:
-        n_dict = sorted(n_dict, key=itemgetter('Rating'),reverse=True) 
+        n_dict = sorted(n_dict, key=itemgetter('Rating'),reverse=True)
+    if R!=None:
+        temp=[]
+        for i in n_dict:
+            if i['Rating']>=R:
+                temp.append(i)
+        n_dict=temp 
     head_row=['Prob','Rating']
     return head_row,n_dict
