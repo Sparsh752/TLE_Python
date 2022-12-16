10d0e009dd576c30a977653035e22f22fa9a2726
sparshmittal <sparshmittal2311@gmail.com>
2022-12-16 22:47:15 +0530

    changes

--- responses.py
+++ responses.py
@@ -13,11 +13,6 @@ async def handle_response(message,ctx):
         return str(random.randint(1,6))
     if p_message=="!help":
         return "Commands: hello, roll"
-    if p_message=="!next":                      #gives list of next contests on codeforces
-        return clist_api.nextcontests()         #fetches next contests
-    if msg_data[0]=="!rating_changes":
-        if len(msg_data)==2:
-            return await contest_info.codeforces_rating_changes(msg_data[1])
     if msg_data[0]==";gitgud":
         return await gitgud.gitgud(ctx)
     if msg_data[0]==";gotgud":
