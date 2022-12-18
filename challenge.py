f0788c9528409b0f0a79061358a85e8e35ef1a02
sparshmittal <sparshmittal2311@gmail.com>
2022-12-18 12:45:00 +0530

    fixes

--- challenge.py
+++ challenge.py
@@ -23,14 +23,14 @@ async def challenge_question_cf(ctx,bot):
     if len(ctx.mentions) == 0:
             await msg.edit(content=f"{ctx.author.mention} The mentioned user can't be challenged")
             return
+    if discord_id == ctx.author.id:
+        await msg.edit(content=f"{ctx.author.mention} You cannot challenge yourself")
+        return
+    if discord_id == bot.user.id:
+        await msg.edit(content=f"{ctx.author.mention} You cannot challenge me")
+        return
     if(user_message[1]=='cf'):
         discord_id = ctx.mentions[0].id
-        if discord_id == ctx.author.id:
-            await msg.edit(content=f"{ctx.author.mention} You cannot challenge yourself")
-            return
-        if discord_id == bot.user.id:
-            await msg.edit(content=f"{ctx.author.mention} You cannot challenge me")
-            return
         ctx_second=cttx()
         ctx_second.author.id=discord_id
         cf_handle_1 = await get_codeforces_handle(ctx)
